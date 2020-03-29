d0d39ae3cf7879f9b23f637cb1a2c2c273a0d362
Srinidhi <srinidhi.prabandham@sycamoreinformatics.com>
2020-03-29 15:36:06 +0530

    modifications to web and landing page

---
+++ web/src/containers/LandingPageContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class LandingPageContainer extends Component {
+    render() {
+        return (
+            <h1 className="text-center mt-5">Welcome to Larks Framework !!</h1>
+        )
+    }
+}
--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import LandingPageContainer from "./containers/LandingPageContainer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPageContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
